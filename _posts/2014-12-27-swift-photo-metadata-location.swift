1d2cb7788af27c90e08a39511917f4bb71c3e843
Aleksandr Salo <alexsalo@users.noreply.github.com>
2015-01-03 15:19:58 -0600

    Update 2014-12-27-swift-photo-metadata-location.swift

--- _posts/2014-12-27-swift-photo-metadata-location.swift
+++ _posts/2014-12-27-swift-photo-metadata-location.swift
@@ -2,17 +2,8 @@
 layout: post
 title: You're up and running!
 ---
-{% highlight ruby linenos %}
-def show
-  puts "Outputting a very lo-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-ong lo-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-ong line"
-  @widget = Widget(params[:id])
-  respond_to do |format|
-    format.html # show.html.erb
-    format.json { render json: @widget }
-  end
-end
-{% endhighlight %}
-
+The purpose of this block of code is to print to the console the city and region where the photo, selected via standard ios image picker controller, was taken.
+In this example I used ALAssets
 {% highlight haskell linenos %}
 //UIImagePickerControllerDelegate Methods
 func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!){
