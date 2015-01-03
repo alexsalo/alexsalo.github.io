a8fcf6bab8b14989f9a6344722f87e5f06d2aafd
Aleksandr Salo <alexsalo@users.noreply.github.com>
2015-01-03 15:16:06 -0600

    Update 2014-12-27-swift-photo-metadata-location.swift

--- _posts/2014-12-27-swift-photo-metadata-location.swift
+++ _posts/2014-12-27-swift-photo-metadata-location.swift
@@ -1,7 +1,7 @@
-The purpose of this block of code is to print to the console the city and region where the photo, 
-selected via standard ios image picker controller, was taken.
-
-
+---
+layout: post
+title: You're up and running!
+---
 {% highlight ruby linenos %}
 def show
   puts "Outputting a very lo-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-ong lo-o-o-o-o-o-o-o-o-o-o-o-o-o-o-o-ong line"
