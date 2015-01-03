bd7d09d45ca0734300c496394a4005b91b28e9e2
Aleksandr Salo <alexsalo@users.noreply.github.com>
2015-01-03 13:16:22 -0600

    Update 2014-12-27-swift-photo-metadata-location.swift

--- _posts/2014-12-27-swift-photo-metadata-location.swift
+++ _posts/2014-12-27-swift-photo-metadata-location.swift
@@ -12,3 +12,24 @@ def show
   end
 end
 {% endhighlight %}
+
+{% highlight haskell linenos %}
+//UIImagePickerControllerDelegate Methods
+func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!){
+    let library = ALAssetsLibrary()
+    let URL: AnyObject? = info["UIImagePickerControllerReferenceURL"]
+    var error: NSError?
+    library.assetForURL(URL as NSURL, resultBlock: { (asset) -> Void in
+        let location = asset.valueForProperty(ALAssetPropertyLocation) as? CLLocation
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location!) { (data, error) -> Void in
+            let address = data[0].addressDictionary!
+            let city = address[kABPersonAddressCityKey] as  NSString!
+            let state = address[kABPersonAddressStateKey] as  NSString!
+            println(city, state)
+        }
+    }) { (error) -> Void in
+        println(error)
+    }
+}
+{% endhighlight %}
